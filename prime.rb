b5ef38152ae8d0ccbdd53e2955979d563bbb8c88
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-10 09:25:13 -0600

    commit tree 60e92bc032b2332236b66a1d757817f79de2edbf

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(num)
+    if num < 2
+        false
+    else
+        (2...num).to_a.none? do |range_element|
+            return false if num % range_element == 0
+        end
+    end
+ end 
